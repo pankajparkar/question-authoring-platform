570c5911ff7c6f6d3396589a7b90feedd464aed8
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-07-15 01:53:01 +0530

    UI of question builder component

---
+++ src/app/question-builder/question-builder.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qap-question-builder',
+  templateUrl: './question-builder.component.html',
+  styleUrls: ['./question-builder.component.css']
+})
+export class QuestionBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  favoriteSeason: string;
+  questionTypes: any[] = [
+    {id: 1, name: 'Multiple choice question', code: 'mcq'},
+    {id: 2, name: 'Submission type question', code: 'submission'},
+    {id: 3, name: 'Passage(text) type question', code: 'passage'},
+  ]
+
+  ngOnInit() {
+  }
+
+}
